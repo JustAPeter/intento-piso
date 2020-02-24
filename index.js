0ca62aa56b51f50e582143986f8f2698d6682813
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2020-02-23 22:36:15 -0500

    base

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        botones:[
+            {
+                nombre: 'SALA 1',
+                estado: false
+            }
+        ]
+    },
+    methods: {
+
+    }
+})
