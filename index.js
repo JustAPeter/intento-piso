9e82cc38fde130fdb0673c5d59057730ca441353
JustAPeter <47680127+JustAPeter@users.noreply.github.com>
2020-02-28 23:10:05 -0500

    No sirvo pa ni mierda, perdon

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var app = new Vue({
-    el: '#app',
+    el: "body",
     data: {
         eleccion:  null,
-        botones:[
-            {
-                nombre: 'SALA 1',
-                estado: false
-            }
-        ]
     },
     methods: {
         luz(sala){
-            botones.forEach(element => {if(element.nombre === sala) eleccion=element.estado } );
-
+            botones.forEach(element => {if(element.nombre === sala) {eleccion=element.estado; console.log(element.estado)} } );
+            
         },
         cambio(sala){
             botones.forEach(element => {if(element.nombre === sala){ if(element.estado === true) {element.estado = false} else{element.estado=true} }})
         }
     }
 })
+
+var botones = [
+    {
+        nombre: 'SALA 1',
+        estado: false
+    }
+]
+
+var yellow = false
